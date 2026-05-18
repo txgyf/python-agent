fc87ae6da76bbb19a614b08de357b7453eca8d69
管伊帆 <15110761762@163.com>
2026-05-18 21:06:05 +0800

    feat: add database configuration

---
+++ app/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/app.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
